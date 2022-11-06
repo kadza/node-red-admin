b7ada6d888fee7b144a3b79e4cc164e32c8638b1
Lukasz Kujawiak <lukasz.kujawiak@gmail.com>
2022-11-06 11:30:29 +0000

    ssh keys work

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
 set -e
 
-cp -R /tmp/.ssh /usr/src/node-red/.ssh
-chmod 700 /usr/src/node-red/.ssh
-chmod 644 /usr/src/node-red/.ssh/id_rsa.pub
-chmod 600 /usr/src/node-red/.ssh/id_rsa
+mkdir -p /data/projects/.sshkeys
+# I wanted to use symbolic links in dockerfile, but node-red admin expects a file, so copying necessary
+# node-red expects files to start with username
+cp /run/secrets/user_ssh_key /data/projects/.sshkeys/admin_id_rsa
+cp /run/secrets/user_ssh_pub_key /data/projects/.sshkeys/admin_id_rsa.pub
+# when I cused COPY in dockerfile, the files weren't copied
+cp /tmp/settings.js /data/settings.js
+cp /tmp/.npmrc /data/.npmrc
 
 npm --no-update-notifier --no-fund start --cache /data/.npm -- --userDir /data
